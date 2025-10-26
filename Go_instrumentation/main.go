d5175bc94bbcc4f6750cdf2ffd4ca31c8423104b
priyadarshanchougule <priyadarshanchougule@gmail.com>
2025-10-26 16:36:00 +0530

    build

--- Go_instrumentation/main.go
+++ Go_instrumentation/main.go
@@ -4,17 +4,32 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func check() {
+var REQUEST_COUNT = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "go_app_total_request_count",
+	Help: "total http request count",
+})
+
+func main() {
 	// Create a new Gin router with default middleware (logger and recovery)
 	router := gin.Default()
+
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(collectors.NewGoCollector())
+	reg.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+	reg.MustRegister(REQUEST_COUNT)
 
 	// Example: root route
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Welcome to the Gin server!",
 		})
+		REQUEST_COUNT.Inc()
 	})
 
 	// Example: simple API route
@@ -22,6 +37,7 @@ func check() {
 		c.JSON(http.StatusOK, gin.H{
 			"response": "pong",
 		})
+		REQUEST_COUNT.Inc()
 	})
 
 	// Example: POST route
@@ -35,6 +51,7 @@ func check() {
 			"received": requestBody,
 		})
 	})
+	router.GET("/metrics", gin.WrapH(promhttp.HandlerFor(reg, promhttp.HandlerOpts{})))
 
 	// Start server on port 8080
 	router.Run(":8080")
